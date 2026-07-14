c7034f3f9afaa1caeaa98aeaa1531dc248d7de69
Joshua Campbell <joshcampbell191@users.noreply.github.com>
2026-07-14 19:10:47 -0400

    Update funding.rb

--- .github/workflows/updater/lib/github/funding.rb
+++ .github/workflows/updater/lib/github/funding.rb
@@ -5,16 +5,18 @@ require 'yaml'
 module GitHub
   # Funding information for a GitHub repository
   class Funding
+    BUY_ME_A_COFFEE = 'buy_me_a_coffee'
     GITHUB = 'github'
     KO_FI = 'ko_fi'
     PATREON = 'patreon'
     CUSTOM = 'custom'
 
+    BUY_ME_A_COFFEE_URL = 'https://buymeacoffee.com'
     GITHUB_URL = 'https://github.com/sponsors'
     KO_FI_URL = 'https://ko-fi.com'
     PATREON_URL = 'https://www.patreon.com'
 
-    attr_accessor :github, :ko_fi, :patreon, :custom
+    attr_accessor :buy_me_a_coffee, :github, :ko_fi, :patreon, :custom
 
     def initialize(funding)
       funding.each do |platform, value|
@@ -33,11 +35,16 @@ module GitHub
 
     def parse_platform(platform, value)
       case platform
+      when BUY_ME_A_COFFEE then parse_buy_me_a_coffee(value)
       when GITHUB then parse_github(value)
       when KO_FI then parse_ko_fi(value)
       when PATREON then parse_patreon(value)
       when CUSTOM then parse_custom(value)
       end
+    end
+    
+    def parse_buy_me_a_coffee(username)
+      @buy_me_a_coffee = "#{BUY_ME_A_COFFEE_URL}/#{username}"
     end
 
     def parse_github(value)
